75819f55a8e44759e16385fdb11fbcc3bd04a94a
OkashiKami <zbigmoney305@gmail.com>
2023-09-22 16:13:31 -0500

    Add project files.

---
+++ Xngine/Xngine.cpp
@@ -0,0 +1,11 @@
+// Xngine.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <SDL2/SDL.h>
+#include <vulkan/vulkan.h>
+#include <iostream>
+
+int main()
+{
+    
+    return 0;
+}
